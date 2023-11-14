73b5d236492a28a28c2d5ea5f80c6ab75ec4cbb9
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-11-14 12:36:43 +0530

    Added RDoc for modules and classes

--- lib/unit_measurements/rails/unit_groups/all.rb
+++ lib/unit_measurements/rails/unit_groups/all.rb
@@ -1,7 +1,3 @@
 # -*- encoding: utf-8 -*-
 # -*- frozen_string_literal: true -*-
 # -*- warn_indent: true -*-
-
-require "unit_measurements/rails/base"
-
-require "unit_measurements/rails/unit_groups/all"
